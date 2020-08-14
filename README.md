1209c3815d1323f11b3def8e3a7c826be0be5a56
Nicholas La Monaco <nlamonaco86@gmail.com>
2020-08-14 12:01:08 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# README Generator
+
+    ## Description:
+    A simple, command line application to quickly and easily generate a high quality README.md file. 
+
+
+
+    ## Installation Instructions:
+    Clone the repo to your computer, then install the necessary dependencies. After that, simply run rGen.js and answer the prompts to create a README!
+
+    ## Usage Info:
+    Feel free to try it out!
+
+    ## Contributing:
+    Nicholas La Monaco 8/14/2020
+
+    ## Testing Instructions:
+    There are no test files for this application, though suggestions are always welcome
+
+    ## Contact Me!
+    Github Repo: github.com/nlamonaco86/rGen
+
+    View my Github at github.com/nlamonaco86/
+
+    Have a question? E-mail me at nlamonaco86@gmail.com
+
+    This README was generated with my rGen CLI app!
+    
+    ## Licensing Info:
+    MIT
---
+++ rGen.js
@@ -0,0 +1,96 @@
+//dependencies
+const inquirer = require("inquirer");
+const fs = require('fs');
+
+//ask questions to determine README content
+inquirer.prompt([
+    {
+        type: "input",
+        message: "Project Title:",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Github Repo Address:",
+        name: "repo",
+    },
+    {
+        type: "input",
+        message: "Github Username:",
+        name: "username",
+    },
+    {
+        type: "input",
+        message: "E-Mail Address:",
+        name: "email",
+    },
+    {
+        type: "editor",
+        message: "Description:",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Installation Instructions:",
+        name: "install",
+    },
+    {
+        type: "input",
+        message: "Usage Info:",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "Contributing:",
+        name: "contrib",
+    },
+    {
+        type: "input",
+        message: "Testing Instructions:",
+        name: "test",
+    },
+
+    {
+        type: "list",
+        message: "Please choose a license for your project.",
+        choices: ["MIT", "Apache License 2.0", "GNU", "BSD 2-Clause 'Simplified'", "BSD 3-Clause 'New'", "None"],
+        name: "license",
+    }
+    //fswritefile (expects name, data and callback)
+]).then(function (response) {
+//use template literal to layout and write the README
+    fs.writeFile("README.md",
+    `# ${response.title}
+
+    ## Description:
+    ${response.description}
+
+    ## Installation Instructions:
+    ${response.install}
+
+    ## Usage Info:
+    ${response.usage}
+
+    ## Contributing:
+    ${response.contrib}
+
+    ## Testing Instructions:
+    ${response.test}
+
+    ## Contact Me!
+    Github Repo: ${response.repo}
+
+    View my Github at github.com/${response.username}/
+
+    Have a question? E-mail me at ${response.email}
+
+    This README was generated with my rGen CLI app!
+    
+    ## Licensing Info:
+    ${response.license}`,
+    function (err) {
+        if (err) return console.log(err);
+        console.log("success");
+    }
+)});
+
